632fbe29cc394a7810f5dc92aa5fc5b76af28c71
George Phillips <georgepaulphillips@gmail.com>
2018-03-22 15:36:51 +1300

    added cloudcannon suite

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const suite = require("cloudcannon-suite");
+
+suite.jekyllDev(gulp, {
+	serve: {
+		port: 4001
+	}
+});
+
+suite.screenshots(gulp);
+
+suite.help(gulp);
+
+gulp.task("default", ["help"]);
